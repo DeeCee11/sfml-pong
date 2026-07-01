0c3b962be617d227d24de5d6a5d5dd19aebc8002
DC <donncha.mccooey1105@gmail.com>
2026-07-01 12:00:38 +0100

    add walls + divider

--- README.md
+++ README.md
@@ -4,7 +4,7 @@ This is a recreation of the classic arcade game Pong with SFML/C++, created as m
 
 # Goals
 ## Basic goals
-- [ ] Arena with 2 walls + divider
+- [x] Arena with 2 walls + divider
 - [ ] User-controllable paddles
 - [ ] Ball that moves around play field + bounces off paddles/walls
 - [ ] Detect when ball leaves play field
--- src/main.cpp
+++ src/main.cpp
@@ -2,12 +2,25 @@
 
 int main()
 {
-	sf::RenderWindow window( sf::VideoMode( { 200, 200 } ), "SFML works!" );
-	sf::CircleShape shape( 100.f );
-	shape.setFillColor( sf::Color::Green );
+	const int WINDOW_WIDTH = 512;
+	const int WINDOW_HEIGHT = 480;
+	const float WALL_THICKNESS = 10;
+	const float DIVIDER_THICKNESS = 3;
+
+	sf::RenderWindow window( sf::VideoMode( { WINDOW_WIDTH, WINDOW_HEIGHT } ), "Pong" );
+
+	// Walls
+	sf::RectangleShape top_wall({WINDOW_WIDTH, WALL_THICKNESS});
+	sf::RectangleShape bottom_wall({WINDOW_WIDTH, WALL_THICKNESS});
+	bottom_wall.setPosition({0, WINDOW_HEIGHT-WALL_THICKNESS});
+
+	// Divider
+	sf::RectangleShape divider({DIVIDER_THICKNESS, WINDOW_HEIGHT});
+	divider.setPosition({WINDOW_WIDTH/2 - (DIVIDER_THICKNESS/2), 0});
 
 	while ( window.isOpen() )
 	{
+		// Handle window close
 		while ( const std::optional event = window.pollEvent() )
 		{
 			if ( event->is<sf::Event::Closed>() )
@@ -15,7 +28,11 @@ int main()
 		}
 
 		window.clear();
-		window.draw( shape );
+
+		window.draw(top_wall);
+		window.draw(bottom_wall);
+		window.draw(divider);
+
 		window.display();
 	}
 }
